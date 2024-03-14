c6abebe82bb18b51b1b28db36afb4bd2cefd12d9
dell <Puspanjalikhora123@gmail.com>
2024-03-14 14:14:49 +0530

    Remove one method

--- src/test/java/testCases/Testclass.java
+++ src/test/java/testCases/Testclass.java
@@ -7,18 +7,5 @@ public class Testclass
 	{
 		System.out.println("This is my first Test1 code");
 	}
- public  void Test2()
-	
-	{
-		System.out.println("This is my Test2 code");
-	}
-
-  public  void Test3()
-	
-	{
-		System.out.println("This is my Test3 code");
-	}
-
-	}
-
+}
 
