5b91273af475e323db9781ed8fc9b28b6003a45a
dell <Puspanjalikhora123@gmail.com>
2024-03-14 14:12:57 +0530

    Added one more Test class

---
+++ src/test/java/testCases/Testclass.java
@@ -0,0 +1,24 @@
+package testCases;
+
+public class Testclass 
+{
+	public  void Test1()
+	
+	{
+		System.out.println("This is my first Test1 code");
+	}
+ public  void Test2()
+	
+	{
+		System.out.println("This is my Test2 code");
+	}
+
+  public  void Test3()
+	
+	{
+		System.out.println("This is my Test3 code");
+	}
+
+	}
+
+
